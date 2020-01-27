be89a0b2e20352631778a868052aff26698d50e5
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-27 17:06:59 -0500

    finished advanced testing environment setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! I am pleased to have met you"
   end
 
   def goodbye
-    render html: "goodbye, world!"
+    render html: "goodbye, world! The pleasure was all mine."
   end
 end
